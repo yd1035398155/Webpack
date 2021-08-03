4900c8d28367ec253d5ca6b3b5e2f18e5ca8fe26
Wizard <1035398155@qq.com>
2021-08-03 12:49:20 +0800

    打包图片资源

--- 05.打包图片资源/build/main.js
+++ 05.打包图片资源/build/main.js
@@ -36,7 +36,7 @@ eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var _ind
   \***************************/
 /***/ ((module, __unused_webpack_exports, __webpack_require__) => {
 
-eval("module.exports = __webpack_require__.p + \"3e45bff73c7625c5016e.jpg\";\n\n//# sourceURL=webpack://webpack_test/./src/recommend.jpg?");
+eval("module.exports = __webpack_require__.p + \"img/recommend.3e45bff7.jpg\";\n\n//# sourceURL=webpack://webpack_test/./src/recommend.jpg?");
 
 /***/ }),
 
@@ -194,18 +194,6 @@ eval("\n\n/* istanbul ignore next  */\nfunction styleTagTransform(css, style) {\
 /******/ 		};
 /******/ 	})();
 /******/ 	
-/******/ 	/* webpack/runtime/global */
-/******/ 	(() => {
-/******/ 		__webpack_require__.g = (function() {
-/******/ 			if (typeof globalThis === 'object') return globalThis;
-/******/ 			try {
-/******/ 				return this || new Function('return this')();
-/******/ 			} catch (e) {
-/******/ 				if (typeof window === 'object') return window;
-/******/ 			}
-/******/ 		})();
-/******/ 	})();
-/******/ 	
 /******/ 	/* webpack/runtime/hasOwnProperty shorthand */
 /******/ 	(() => {
 /******/ 		__webpack_require__.o = (obj, prop) => (Object.prototype.hasOwnProperty.call(obj, prop))
@@ -224,22 +212,7 @@ eval("\n\n/* istanbul ignore next  */\nfunction styleTagTransform(css, style) {\
 /******/ 	
 /******/ 	/* webpack/runtime/publicPath */
 /******/ 	(() => {
-/******/ 		var scriptUrl;
-/******/ 		if (__webpack_require__.g.importScripts) scriptUrl = __webpack_require__.g.location + "";
-/******/ 		var document = __webpack_require__.g.document;
-/******/ 		if (!scriptUrl && document) {
-/******/ 			if (document.currentScript)
-/******/ 				scriptUrl = document.currentScript.src
-/******/ 			if (!scriptUrl) {
-/******/ 				var scripts = document.getElementsByTagName("script");
-/******/ 				if(scripts.length) scriptUrl = scripts[scripts.length - 1].src
-/******/ 			}
-/******/ 		}
-/******/ 		// When supporting browsers where an automatic publicPath is not supported you must specify an output.publicPath manually via configuration
-/******/ 		// or pass an empty string ("") and set the __webpack_public_path__ variable from your code to use your own logic.
-/******/ 		if (!scriptUrl) throw new Error("Automatic publicPath is not supported in this browser");
-/******/ 		scriptUrl = scriptUrl.replace(/#.*$/, "").replace(/\?.*$/, "").replace(/\/[^\/]+$/, "/");
-/******/ 		__webpack_require__.p = scriptUrl;
+/******/ 		__webpack_require__.p = "./";
 /******/ 	})();
 /******/ 	
 /******/ 	/* webpack/runtime/jsonp chunk loading */
--- 05.打包图片资源/webpack.config.js
+++ 05.打包图片资源/webpack.config.js
@@ -1,4 +1,5 @@
 // 注意:webpack5自动处理图片资源,
+// https://blog.csdn.net/qq_45178972/article/details/117631747
 const { resolve } = require("path")
 const HtmlWebpackPlugin = require("html-webpack-plugin")
 module.exports = {
@@ -6,6 +7,7 @@ module.exports = {
   output: {
     filename: "main.js",
     path: resolve(__dirname, "build"),
+    publicPath: "./",
   },
   module: {
     rules: [
@@ -14,6 +16,7 @@ module.exports = {
         use: ["style-loader", "css-loader", "less-loader"],
       },
       {
+        // 问题:处理不了html中的img图片
         test: /\.(png|jpg|gif)$/i,
         type: "asset",
         parser: {
@@ -21,6 +24,16 @@ module.exports = {
             maxSize: 8 * 1024, // 8kb  指定大小
           },
         },
+        generator: {
+          // 在出口文件夹下创建img文件夹,图片名字为原来的名字+.8位哈希值+图片扩展名
+          filename: "img/[name].[hash:8][ext]",
+        },
+      },
+      {
+        // 解决上面问题
+        test: /\.html$/,
+        // 处理html文件的img图片(负责引入img,从而能被asset处理)
+        loader: "html-loader",
       },
     ],
   },
