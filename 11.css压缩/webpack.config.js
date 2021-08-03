09e000d954d5df18c81f658aedd99ad62c5c95f7
Wizard <1035398155@qq.com>
2021-08-03 16:19:37 +0800

    css兼容性处理

--- 11.css压缩/webpack.config.js
+++ 11.css压缩/webpack.config.js
@@ -1,9 +1,7 @@
 const { resolve } = require("path")
 const HtmlWebpackPlugin = require("html-webpack-plugin")
-// 1.引入插件
 const MiniCssExtractPlugin = require("mini-css-extract-plugin")
 
-// 设置nodejs环境变量
 process.env.NODE_ENV = "development"
 module.exports = {
   entry: "./src/js/index.js",
@@ -15,30 +13,9 @@ module.exports = {
     rules: [
       {
         test: /\.css$/,
-        // 3.替换style-loader
         use: [
           MiniCssExtractPlugin.loader,
           "css-loader",
-          /**
-           * css兼容性处理:postcss-->postcss-loader postcss-preset-env
-           * 帮postcss找到package.json中的browserslist里面的配置,通过配置加载指定的css兼容性样式
-					 * 
-					 * 
-					   "browserslist": {
-							默认的环境是生产环境,需要在本文件开头设置为开发环境-->process.env.NODE_ENV = "development"
-							
-    				 "development": [
-    				   "last 1 chrome version",
-    				   "last 1 firefox version",
-    				   "last 1 safari version"
-    				 ],
-    				 "production": [
-    				   ">0.2%",
-    				   "not dead",
-    				   "not op_mini all"
-    				 ]
-  					}
-           */
           {
             loader: "postcss-loader",
             options: {
@@ -55,7 +32,6 @@ module.exports = {
     new HtmlWebpackPlugin({
       template: "./src/index.html",
     }),
-    // 2.使用插件
     new MiniCssExtractPlugin({
       filename: "css/index.css",
     }),
